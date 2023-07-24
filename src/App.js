38133798c2469047877386f70f72737c831fae5f
Filza Khan Tareen <filzatareen@gmail.com>
2023-07-24 21:05:08 +0500

    button styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header";
+import Search from "./Components/Search";
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Search />
     </div>
   );
 }
---
+++ src/Components/Search.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+function Search() {
+const [searchVal,setSearchVal] = useState('');
+
+    return (
+        <div className=' mt-14 p-10'>
+            <form className='flex justify-center sm:justify-start gap-5'>
+                <input className='w-[300px] h-11 sm:w-[400px] rounded-md font-semibold p-4 focus:bg-[#2C3639] focus:duration-700 text-xl text-[#DCD7C9]'
+                    type='text' placeholder='Search for movies' name='searchInp'>
+                </input>
+
+                <button className='flex gap-1 items-center border-b-4 border-[#3F4E4F] hover:border-[#A27B5C] pr-3 group hover:duration-700 hover:translate-x-4'
+                 name='searchBtn'>
+                    <svg class="h-8 w-8 text-[#A27B5C] group-hover:translate-x-1 hover:duration-700" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <polyline points="13 17 18 12 13 7" />
+                        <polyline points="6 17 11 12 6 7" />
+                    </svg>
+                    Search
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default Search
